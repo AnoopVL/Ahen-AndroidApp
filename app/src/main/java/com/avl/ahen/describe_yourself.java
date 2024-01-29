aca7d78261ecdbbf351b46b61969dd5b4a212fcc
AnoopVL <avlanjekar4@gmail.com>
2024-01-30 01:20:05 +0530

    describeYourself

---
+++ app/src/main/java/com/avl/ahen/describe_yourself.java
@@ -0,0 +1,14 @@
+package com.avl.ahen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class describe_yourself extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_describe_yourself);
+    }
+}
