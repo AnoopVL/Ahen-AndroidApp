2c8987bdb11f905bd46d4936cf0fc8bdae730ee5
AnoopVL <avlanjekar4@gmail.com>
2024-02-01 01:19:17 +0530

    user Firebase auth

--- app/src/main/java/com/avl/ahen/user_login.java
+++ app/src/main/java/com/avl/ahen/user_login.java
@@ -1,31 +1,82 @@
 package com.avl.ahen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class user_login extends AppCompatActivity {
 
    private Button login;
    private TextView signup;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+    TextInputEditText usrEmail, usrPassword;
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(user_login.this, userDashoard.class);
+            startActivity(intent);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
 
+
         login = findViewById(R.id.userLoginBtn);
         signup = findViewById(R.id.createAcc);
+        usrEmail = findViewById(R.id.userEmail);
+        usrPassword = findViewById(R.id.userPassword);
+        progressBar = findViewById(R.id.progressBar);
+        mAuth = FirebaseAuth.getInstance();
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(user_login.this, userDashoard.class);
-                startActivity(intent);
+                progressBar.setVisibility(View.VISIBLE);
+                String email = String.valueOf(usrEmail.getText());
+                String password = String.valueOf(usrPassword.getText());
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(user_login.this, "Enter email", Toast.LENGTH_SHORT).show();
+                }
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(user_login.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                }
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(user_login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(user_login.this, userDashoard.class);
+                                    startActivity(intent);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(user_login.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
             }
         });
 
--- app/src/main/java/com/avl/ahen/user_signUp.java
+++ app/src/main/java/com/avl/ahen/user_signUp.java
@@ -1,29 +1,76 @@
 package com.avl.ahen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class user_signUp extends AppCompatActivity {
-    private Button signup;
+    private Button signupBtn;
     private TextView login;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+
+
+    TextInputEditText usrEmail, usrPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_sign_up);
 
-        signup = findViewById(R.id.userSignUpBtn);
-        signup.setOnClickListener(new View.OnClickListener() {
+        signupBtn = findViewById(R.id.userSignUpBtn);
+        usrEmail = findViewById(R.id.userEmail);
+        usrPassword = findViewById(R.id.userPassword);
+        progressBar = findViewById(R.id.progressBar);
+        mAuth = FirebaseAuth.getInstance();
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(user_signUp.this, user_login.class);
-                Toast.makeText(user_signUp.this, "Account Created !", Toast.LENGTH_SHORT).show();
-                startActivity(intent);
+                progressBar.setVisibility(View.VISIBLE);
+                String email = String.valueOf(usrEmail.getText());
+                String password = String.valueOf(usrPassword.getText());
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(user_signUp.this, "Enter email", Toast.LENGTH_SHORT).show();
+                }
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(user_signUp.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                }
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(user_signUp.this, "Account Created !", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(user_signUp.this, user_login.class);
+                                    startActivity(intent);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(user_signUp.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
             }
         });
         login = findViewById(R.id.alrdLogin);
@@ -34,6 +81,8 @@ public class user_signUp extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 
 }
